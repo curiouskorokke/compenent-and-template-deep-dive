82a203f9e17594703fff64b877c73e10a3668299
curiouskorokke <nicholasngshihhao@gmail.com>
2025-07-05 17:58:21 +0800

    control component with input and text area content projection

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+})
+export class ControlComponent {
+  @Input({ required: true }) label!: string;
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { ControlComponent } from './control/control.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, ControlComponent],
   imports: [CommonModule],
-  exports: [ButtonComponent],
+  exports: [ButtonComponent, ControlComponent],
 })
 export class SharedModule {}
